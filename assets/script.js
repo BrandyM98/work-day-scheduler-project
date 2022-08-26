9ea7fbae0897810d673aafcefa531de20b293356
Brandy Moore <brandymoore923@gmail.com>
2022-08-26 09:28:00 -0500

    was able to fix local storage and fixed button to bootstrap in html

--- assets/script.js
+++ assets/script.js
@@ -3,44 +3,47 @@
 let currentHour = moment().format("HH");
 
 $(document).ready(function () {
-    let timeNow = moment().format("MMMM Do YYYY");
-    let displayDate = document.getElementById("currentDay");
-    displayDate.innerHTML = timeNow;
+  let timeNow = moment().format("MMMM Do YYYY");
+  let displayDate = document.getElementById("currentDay");
+  displayDate.innerHTML = timeNow;
 });
 
 //clear local storage
 $("#clearBtn").click(function (event) {
-    event.preventDefault;
-    $("textarea").val("");
-    localStorage.clear();
-  });
+  event.preventDefault;
+  $("textarea").val("");
+  localStorage.clear();
+});
 
-  //----//
-  $(".time-div").each(function () {
-    var timeDiv = $(this).attr("id").split("-")[1];
-    
-    if (currentHour == timeDiv) {
-      $(this).addClass("present");
-      $(this).children(".text").addClass("white-text");
-    } else if (currentHour < timeDiv) {
-      $(this).removeClass("present");
-      $(this).addClass("future");
-    } else if (currentHour > timeDiv) {
-      $(this).removeClass("future");
-      $(this).addClass("past");
-    }
-  });
+// Gets the `id` from the `.time-div` element
+  // finds each time divs element and gets the text from the matching key 
+
+$(".time-div").each(function () {
+  var timeDiv = $(this).attr("id").split("-")[1];
+  
+  if (currentHour == timeDiv) {
+    $(this).addClass("present");
+    $(this).children(".text").addClass("white-text");
+  } else if (currentHour < timeDiv) {
+    $(this).removeClass("present");
+    $(this).addClass("future");
+  } else if (currentHour > timeDiv) {
+    $(this).removeClass("future");
+    $(this).addClass("past");
+  }
+});
+
 
 //save local storage
 $(".saveBtn").click(function (event) {
-    event.preventDefault();
-    var value = $(this).siblings(".time-text").val();
-    var time = $(this).parent().attr("id").split("-")[0];
-    localStorage.setItem(time, value);
-
-    //console.log(time);
+  event.preventDefault();
+  var value = $(this).siblings(".time-text").val();
+  var time = $(this).parent().attr("id").split("-")[0];
+  localStorage.setItem(time, value);
 });
 
+
+//console.log(time);
 
 $("#hour09 .time-text").val(localStorage.getItem("hour09"));
 $("#hour10 .time-text").val(localStorage.getItem("hour10"));
